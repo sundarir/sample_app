960efb5090656e7e7975513cb68851542d5c311b
Sundari <sundari@hawk.(none)>
2013-06-14 12:49:02 +0530

    Make a basic User model (including secure passwords)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,55 +1,35 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
 require 'spec_helper'
 
 describe User do
 
   before do
     @user = User.new(name: "Example User", email: "user@example.com", 
-                     password: "foobar", password_confirmation: "foobar") 
+                     password: "foobar", password_confirmation: "foobar")
   end
 
-  subject { @user }
+subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
-  it { should respond_to(:password_digest) }
-    it { should respond_to(:password) }
+   it { should respond_to(:password_digest) }
+     it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-it { should respond_to(:authenticate) }
+   it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
-  describe "when name is not present" do
-    before { @user.name = " " }
-    it { should_not be_valid }
-  end
-
-
-
-
-describe "when email is not present" do
+ describe "when email is not present" do
     before { @user.email = " " }
     it { should_not be_valid }
   end
 
-
-describe "when name is too long" do
+   describe "when name is too long" do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
 
 
-describe "when email format is invalid" do
+  describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
                      foo@bar_baz.com foo@bar+baz.com]
@@ -60,6 +40,7 @@ describe "when email format is invalid" do
     end
   end
 
+
   describe "when email format is valid" do
     it "should be valid" do
       addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
@@ -69,12 +50,9 @@ describe "when email format is invalid" do
       end      
     end
   end
-
-
-describe "when email address is already taken" do
+  describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
-       user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
     end
 
@@ -82,7 +60,17 @@ describe "when email address is already taken" do
   end
 
 
-describe "when password is not present" do
+  describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
+  end
+
+  describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
   end
@@ -97,8 +85,7 @@ describe "when password is not present" do
     it { should_not be_valid }
   end
 
-
-  describe "with a password that's too short" do
+   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
   end
@@ -111,7 +98,6 @@ describe "when password is not present" do
       it { should == found_user.authenticate(@user.password) }
     end
 
-
     describe "with invalid password" do
       let(:user_for_invalid_password) { found_user.authenticate("invalid") }
 
@@ -119,15 +105,4 @@ describe "when password is not present" do
       specify { user_for_invalid_password.should be_false }
     end
   end
-  describe "email address with mixed case" do
-    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
-
-    it "should be saved as all lower-case" do
-      @user.email = mixed_case_email
-      @user.save
-      @user.reload.email.should == mixed_case_email.downcase
-    end
-  end
 end
-
-
