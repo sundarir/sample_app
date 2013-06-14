d963c3b9ddb43dd960503cc261482cdd7e67fec7
Sundari <sundari@hawk.(none)>
2013-06-14 18:16:45 +0530

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,9 @@ subject { @user }
      it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
    it { should respond_to(:authenticate) }
+   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
@@ -116,5 +119,9 @@ subject { @user }
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
     end
+     describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
   end
 end
