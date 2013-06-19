927a06012343027aa213a537b6047504c50596bc
Sundari <sundari@hawk.(none)>
2013-06-19 18:30:54 +0530

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
 
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
@@ -15,14 +22,27 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
   after_validation { self.errors.messages.delete(:password_digest) }
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.from_users_followed_by(self)
+  end
+    def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
+  end
+
    private
 
     def create_remember_token
       self.remember_token = SecureRandom.urlsafe_base64
     end
 
-    def feed
-    # This is preliminary. See "Following users" for the full implementation.
-    Micropost.where("user_id = ?", id)
-  end
+    
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -35,10 +35,19 @@ describe User do
       let(:unfollowed_post) do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
       end
+       let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+      end
 
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
     end
   end
   
@@ -48,17 +57,21 @@ subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
-   it { should respond_to(:password_digest) }
-     it { should respond_to(:password) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-   
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
   it { should respond_to(:admin) }
   it { should respond_to(:feed) }
-  it { should respond_to(:relationships) }
-    
   it { should respond_to(:microposts) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:unfollow!) }
 
 
   it { should be_valid }
@@ -74,6 +87,29 @@ subject { @user }
     it { should be_admin }
   end
 
+describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+
+      describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
+    end
+  end
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+  
 
  describe "when name is not present" do
     before { @user.name = " " }
@@ -163,7 +199,6 @@ subject { @user }
 
     describe "with invalid password" do
       let(:user_for_invalid_password) { found_user.authenticate("invalid") }
-
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
     end
@@ -171,5 +206,8 @@ subject { @user }
     before { @user.save }
     its(:remember_token) { should_not be_blank }
   end
+end
+
+end
   end
 end
